aeca3cfdf983642383632a0a9cfa6e1e762ab488
Mateen Bagheri Asl <mateenbagheri@gmail.com>
2023-04-16 14:04:40 +0330

    initial commit. some minor changes needs to be made in order to put influxdb to work

---
+++ logger/factory.go
@@ -0,0 +1,161 @@
+package logger
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+)
+
+// WriterType represents the type of writer to use for the logger.
+type WriterType uint8
+
+const (
+	// ConsoleWriterType represents a console writer.
+	ConsoleWriterType WriterType = iota
+	// FileWriterType represents a file writer.
+	FileWriterType
+	// DatabaseWriterType represents a database writer.
+	DatabaseWriterType
+)
+
+// Factory is a struct that holds the type, service name, and zerolog.Logger.
+type Factory struct {
+	typ        Type
+	service    string
+	zerologger *zerolog.Logger
+}
+
+// New is a function that creates and returns a new Factory.
+func New(typ Type) *Factory {
+	return &Factory{
+		typ:        typ,
+		zerologger: new(zerolog.Logger),
+	}
+}
+
+// WithWriters is a function that sets the writers for the Factory.
+func (f *Factory) WithWriters(writers ...WriterType) *Factory {
+	switch f.typ {
+	case ZeroLoggerType:
+		logger := zerolog.
+			New(zerolog.MultiLevelWriter(f.zerologWriters(writers...)...)).
+			With().
+			Timestamp().
+			Logger()
+
+		f.zerologger = &logger
+	default:
+		panic("typ not acceptable")
+	}
+
+	return f
+}
+
+// WithServiceName is a function that sets the service name for the Factory.
+func (f *Factory) WithServiceName(name string) *Factory {
+	f.service = name
+
+	return f
+}
+
+// WithLevel is a function that sets the logging level for the Factory.
+func (f *Factory) WithLevel(level Level) *Factory {
+	switch f.typ {
+	case ZeroLoggerType:
+		*f.zerologger = f.zerologger.Level(zerolog.Level(level))
+	default:
+		panic("level not acceptable")
+	}
+
+	return f
+}
+
+// WithStackError is a function that sets the stack error for the Factory.
+func (f *Factory) WithStackError() *Factory {
+	if f.typ == ZeroLoggerType {
+		*f.zerologger = f.zerologger.With().Stack().Logger()
+
+		return f
+	}
+
+	return f
+}
+
+// Build is a function that builds and returns the Logger.
+func (f *Factory) Build() Logger { //nolint
+	switch f.typ {
+	case ZeroLoggerType:
+		*f.zerologger = f.zerologger.With().Timestamp().Logger()
+
+		return &ZeroLogger{Logger: f.zerologger, service: f.service}
+	default:
+		return nil
+	}
+}
+
+// zerologWriters is a function that sets the writers for the zerolog.
+func (f *Factory) zerologWriters(writerTypes ...WriterType) []io.Writer {
+	writers := make([]io.Writer, 0)
+
+	for i := range writerTypes {
+		switch writerTypes[i] {
+		case ConsoleWriterType:
+			f.appendZerologConsoleWriter(&writers)
+		case FileWriterType:
+			f.appendZerologFileWriter(f.service, &writers)
+		case DatabaseWriterType:
+			f.appendZerologInfluxWriter(&writers)
+		default:
+			panic("writer type not acceptable")
+		}
+	}
+
+	return writers
+}
+
+// appendZerologConsoleWriter is a function that appends the console writer.
+func (f *Factory) appendZerologConsoleWriter(writers *[]io.Writer) {
+	writer := zerolog.ConsoleWriter{
+		Out:        os.Stdout,
+		TimeFormat: time.RFC3339,
+	}
+
+	*writers = append(*writers, writer)
+}
+
+// appendZerologFileWriter is a method of Factory type that appends a file writer to the slice of io.Writer pointers.
+// It creates a log file with the given service name under the "logs" directory, and opens it in append mode for writing.
+// If the file does not exist, it creates a new one. The file has read and write permissions for owner and read permission for others.
+// The created *os.File pointer is appended to the writers slice.
+// Parameters:
+// - serviceName: string, the name of the service to include in the log file name.
+// - writers: *[]io.Writer, a pointer to a slice of io.Writer pointers that file writer will be appended to.
+// Returns: None
+// TODO: handle log file rotator by app or by os
+func (f *Factory) appendZerologFileWriter(serviceName string, writers *[]io.Writer) {
+	file, err := os.OpenFile(
+		fmt.Sprintf("%s/%s.log", "logs", serviceName),
+		os.O_APPEND|os.O_CREATE|os.O_WRONLY, //nolint
+		0644,
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	*writers = append(*writers, file)
+}
+
+func (f *Factory) appendZerologInfluxWriter(writers *[]io.Writer) {
+	// TODO: rewrite to add new config rules here
+
+	// config := db.NewInfluxConfig()
+
+	// client := influxdb2.NewClient(config.Address(), config.AuthToken())
+	// api := client.WriteAPI(config.Organization(), config.Bucket())
+	// writer := &influxWriter{client, api}
+
+	// *writers = append(*writers, writer)
+}
---
+++ logger/logger.go
@@ -0,0 +1,108 @@
+package logger
+
+// Logger interface defines the logging methods that are common across different loggers
+type Logger interface {
+	TraceLogger
+	DebugLogger
+	InfoLogger
+	WarnLogger
+	ErrorLogger
+	FatalLogger
+	PanicLogger
+
+	// WithScope returns a new Logger instance with a specified scope added to the context
+	WithScope(scope string) Logger
+}
+
+// Type is a custom type to represent different logger types
+type Type int8
+
+const (
+	// ZeroLoggerType represents the Zero logger implementation
+	ZeroLoggerType Type = iota
+)
+
+// Level represents different logging levels that can be used for filtering logs
+type Level int8
+
+const (
+	// DebugLevel is the logging level for debug logs
+	DebugLevel Level = iota
+	// InfoLevel is the logging level for informational logs
+	InfoLevel
+	// WarnLevel is the logging level for warning logs
+	WarnLevel
+	// ErrorLevel is the logging level for error logs
+	ErrorLevel
+	// FatalLevel is the logging level for fatal logs
+	FatalLevel
+	// PanicLevel is the logging level for panic logs
+	PanicLevel
+
+	// TraceLevel is a special logging level to log trace level logs
+	TraceLevel Level = -1
+)
+
+// TraceLogger defines logging methods for trace level logs
+type TraceLogger interface {
+	Trace(message string)
+	Tracef(format string, args ...any)
+
+	// Tracev logs trace level logs with additional context data in the form of key-value pairs
+	Tracev(message string, keyValue ...any)
+}
+
+// DebugLogger defines logging methods for debug level logs
+type DebugLogger interface {
+	Debug(message string)
+	Debugf(format string, args ...any)
+
+	// Debugv logs debug level logs with additional context data in the form of key-value pairs
+	Debugv(message string, KeyValue ...any)
+}
+
+// InfoLogger defines logging methods for info level logs
+type InfoLogger interface {
+	Info(message string)
+	Infof(format string, args ...any)
+
+	// Infov logs info level logs with additional context data in the form of key-value pairs
+	Infov(message string, keyValue ...any)
+}
+
+// WarnLogger defines logging methods for warn level logs
+type WarnLogger interface {
+	Warn(message string)
+	Warnf(format string, args ...any)
+
+	// Warnv logs warn level logs with additional context data in the form of key-value pairs
+	Warnv(message string, keyValue ...any)
+}
+
+// ErrorLogger defines logging methods for error level logs
+type ErrorLogger interface {
+	Err(err error)
+	Error(message string)
+	Errorf(format string, args ...any)
+
+	// Errorv logs error level logs with additional context data in the form of key-value pairs
+	Errorv(message string, keyValue ...any)
+}
+
+// FatalLogger defines logging methods for fatal level logs
+type FatalLogger interface {
+	Fatal(message string)
+	Fatalf(format string, args ...any)
+
+	// Fatalv logs fatal level logs with additional context data in the form of key-value pairs
+	Fatalv(message string, keyValue ...any)
+}
+
+// PanicLogger defines logging methods for panic level logs
+type PanicLogger interface {
+	Panic(message string)
+	Panicf(format string, args ...any)
+
+	// Panicv logs panic level logs with additional context data in the form of key-value pairs
+	Panicv(message string, keyValue ...any)
+}
